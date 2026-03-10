c5379bc644c692ccff858de6da5cfc8ff74850f8
CloudDevOpsHub <100401787+CloudDevOpsHub@users.noreply.github.com>
2026-03-10 19:30:06 +0530

    Update 1st-jenkins.sh

--- 1st-jenkins.sh
+++ 1st-jenkins.sh
@@ -1,22 +1,16 @@
-#!/bin/bash
+# https://www.jenkins.io/doc/book/installing/linux/ 
 
-# Install JRE
-sudo apt install openjdk-17-jre-headless -y
 
-# Create keyrings folder
-sudo mkdir -p /usr/share/keyrings
+openjdk 21.0.8 2025-07-15
+OpenJDK Runtime Environment (build 21.0.8+9-Debian-1)
+OpenJDK 64-Bit Server VM (build 21.0.8+9-Debian-1, mixed mode, sharing)
 
-# Download Jenkins GPG key
-sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
-  https://pkg.jenkins.io/debian/jenkins.io-2023.key
-
-# Add Jenkins repo
-echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian binary/" | sudo tee \
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update
+sudo apt install jenkins
 
-# Update
-sudo apt-get update -y
 
-# Install Jenkins
-sudo apt-get install jenkins -y
